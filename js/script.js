6b81aaa7b4d1e21bfe3d567e55f459d8595627be
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-07 17:27:54 +0200

    [fix]: js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // 1
-const km = parseInt(prompt('Chilometri da percorrere?'));
 const age = parseInt(prompt('Quanti anni ha il passeggero?'));
 
 const discountUnderage = 0.20;
@@ -9,36 +8,40 @@ const priceKm = 0.21;
 
 let message;
 
-let datiValidi = true;
 
 // 2
-const price = (km * priceKm);
 
-console.log(price);
 
+//3
 if(age < 1){
- message = 'Errore! Hai inserito un valore non valido';
-// in caso di errore il flag diventa false
- datiValidi = false;
-}else if(isNaN(age)){
- message = 'Errore! Devi inserire un numero';
- datiValidi = false;
-}else if(isNaN(km)){
-  message = 'Errore! Devi inserire un numero';
-  datiValidi = false;
-}else{
-  // 3
-  if(age < 18){
-    message = (price - (price * discountUnderage)).toFixed(2);
-  } else if(age > 64){
-    message = (price - (price * discountOver65)).toFixed(2);
-  } else {
-    message = price.toFixed(2);
-  }
+  message = 'Errore! Etè segnalata inferiore a 1';
+  }else if(isNaN(age)){
+  message = 'Errore! Devi inserire un numero in età passeggero';
+  }else{
+    
+    const km = parseInt(prompt('Chilometri da percorrere?'));
+    const price = (km * priceKm);
+    console.log(price);
+
+  if(isNaN(km)){
+    message = 'Errore! Devi inserire un numero nel campo chilometri';
+  }else if(km < 1){
+    message = 'Errore! Devi inserire un numero superiore a 1';
+  }else{
+    if(age < 18){
+      message = (price - (price * discountUnderage)).toFixed(2);
+    } else if(age > 64){
+      message = (price - (price * discountOver65)).toFixed(2);
+    } else {
+      message = price.toFixed(2);
+      
+    }
+    
+    console.log(message);
+    message += ' €'
+}
   
   // 4
-  console.log(message);
-  message += ' €'
 }
 
 
