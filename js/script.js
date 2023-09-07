830e95194e48f4b8d202a7900bbbe6ead2b68de5
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-07 16:42:44 +0200

    [fix]: js

--- js/script.js
+++ js/script.js
@@ -16,15 +16,6 @@ const price = (km * priceKm);
 
 console.log(price);
 
-// 3
-// if(age < 18){
-//   message = (price - (price * discountUnderage)).toFixed(2);
-// } else if(age > 64){
-//   message = (price - (price * discountOver65)).toFixed(2);
-// } else {
-//   message = price.toFixed(2);
-// }
-
 if(age < 1){
  message = 'Errore! Hai inserito un valore non valido';
 // in caso di errore il flag diventa false
